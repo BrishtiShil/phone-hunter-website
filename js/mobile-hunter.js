b49641c48f0b3736aeaa0e00d735b519f440d013
Brishti Shil <brishtishil62@gmail.com>
2022-02-28 21:55:36 +0600

    second commit

---
+++ js/mobile-hunter.js
@@ -0,0 +1,30 @@
+const searchMobile = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    // console.log(searchText);
+    searchField.value = '';
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displaySearchResult(data.data));
+}
+const displaySearchResult = data => {
+    const searchResult = document.getElementById('search-result');
+    data.forEach(data => {
+        console.log(data);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card h-100">
+            <img src="${data.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+               <h3 class="card-title">${data.phone_name}</h3>
+               <h5 class="card-title">${data.brand}</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                additional content. This content is a little bit longer.</p>
+             </div>
+        </div>
+        `;
+        searchResult.appendChild(div);
+    })
+}
