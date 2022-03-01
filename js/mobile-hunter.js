38fafd483590106925a23be0a895cd2ba2341dcd
Brishti Shil <brishtishil62@gmail.com>
2022-03-01 20:54:13 +0600

    fifth commit

--- js/mobile-hunter.js
+++ js/mobile-hunter.js
@@ -1,7 +1,6 @@
 const searchMobile = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    // console.log(searchText);
     searchField.value = '';
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     fetch(url)
@@ -11,8 +10,10 @@ const searchMobile = () => {
 const displaySearchResult = data => {
     const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
-    data?.forEach(data => {
-        // console.log(data);
+    if (data.length == 0) {
+        searchResult.textContent = "show no result found";
+    }
+    data.forEach(data => {
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
@@ -21,7 +22,7 @@ const displaySearchResult = data => {
             <div class="card-body">
                <h3 class="card-title">${data.phone_name}</h3>
                <h5 class="card-title">${data.brand}</h5>
-               <button onclick="loadDataDetail('${data.slug}')">Details</button>
+               <button onclick="loadMobileDetail('${data.slug}')">Details</button>
              </div>
         </div>
         `;
@@ -29,10 +30,24 @@ const displaySearchResult = data => {
     })
 }
 
-const loadDataDetail = phoneId => {
-    // console.log(slug);
+const loadMobileDetail = phoneId => {
     const url = ` https://openapi.programming-hero.com/api/phone/${phoneId}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data));
+        .then(data => displayMobileDetails(data));
+}
+const displayMobileDetails = data => {
+    console.log(data);
+    const mobileDetails = document.getElementById('mobile-details');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <img src="${data.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h3 class="card-title">${data.phone_name}</h3>
+                <h4 class="card-title">${data.releaseDate}</h4>                <h4class="card-title">${data.mainFeatures[storage.chipSet.memory.displaySize]}</h4>
+                <a href="" class="btn btn-primary">Go somewhere</a>
+            </div>
+    `;
+    mobileDetails.appendChild(div);
 }
