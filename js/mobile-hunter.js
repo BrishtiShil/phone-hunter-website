eaae579d285ce752ed7b8ace7628d8421812ad8e
Brishti Shil <brishtishil62@gmail.com>
2022-03-01 17:27:41 +0600

    fourth commit

--- js/mobile-hunter.js
+++ js/mobile-hunter.js
@@ -10,18 +10,29 @@ const searchMobile = () => {
 }
 const displaySearchResult = data => {
     const searchResult = document.getElementById('search-result');
-    data.forEach(data => {
+    searchResult.textContent = '';
+    data?.forEach(data => {
+        // console.log(data);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div class="card h-100">
+        <div class="card h-100 text-center mx-auto">
             <img src="${data.image}" class="card-img-top" alt="...">
             <div class="card-body">
                <h3 class="card-title">${data.phone_name}</h3>
                <h5 class="card-title">${data.brand}</h5>
+               <button onclick="loadDataDetail('${data.slug}')">Details</button>
              </div>
         </div>
         `;
         searchResult.appendChild(div);
     })
+}
+
+const loadDataDetail = phoneId => {
+    // console.log(slug);
+    const url = ` https://openapi.programming-hero.com/api/phone/${phoneId}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data));
 }
